e72b2166b0ade8e2ab22a1126d8f06183a98b09d
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-09-19 11:33:11 -0300

    Rework - NotificationHandler -> NotificationNode - Static funcions and NotifiactionNode singleton - Cancel feature implementation

---
+++ addons/NotificationHandler/scripts/notification_node.gd
@@ -0,0 +1,160 @@
+@tool
+extends Node
+class_name NotificationNode
+
+const NOTIFICATION_ID = 'notificationId'
+
+enum NotificationError {
+	VOID = 1,
+	NO_CONTEXT = 2,
+	NO_SETUP = 3,
+	INVALID_NOTIFICATION_DATA = 4,
+	JSON_ERROR = 5,
+	IMAGE_NOT_FOUND = 6,
+}
+
+static var instance: NotificationNode
+var _plugin
+
+@export var images: Array[Imagen] = []
+@export var channels: Array[Channel] = []
+var on_editor: bool:
+	get():
+		return Engine.is_editor_hint()
+var setup_flag: bool = false  
+
+# # Plugin interfaces # #
+
+## Set up routine.
+func setup() -> void:
+	if _plugin:
+		var channels_array = JSON.stringify(channels.map(func(ch): return ch.as_dictionary()))
+		var images_array = JSON.stringify(images.map(func(i: Imagen): return i.save_name))
+		var res = _plugin.setup(channels_array, images_array)
+		self.show_result(res)
+		setup_flag = true
+
+
+## Send a notification.
+static func trigger_notification(
+	channel: String,
+	notification_data: Notification,
+) -> String:
+	if !was_setted(): return ""
+	if !validate_channel_id(channel): return ""
+	
+	var data: String = notification_data.as_string()
+	var res = instance._plugin.triggerNotification(channel, data)
+	instance.show_result(res)
+	return res
+
+
+## Send a notification loaded from a json.
+static func json_notification(
+	channel: String,
+	json_name: String,
+) -> String:
+	if !was_setted(): return ""
+	if (!validate_channel_id(channel)): return ""
+	var res = instance._plugin.jsonNotification(
+		channel,
+		JSONHandler.get_json_path(json_name),
+	)
+	instance.show_result(res)
+	return res
+
+
+## Schedule a notification.
+static func schedule(
+	channel: String,
+	notification_data: Notification,
+	past_n_seconds: int = 0
+) -> String:
+	if !was_setted(): return ""
+	if !validate_channel_id(channel): return ""
+	
+	var data: String = notification_data.as_string()
+	var res = instance._plugin.schedule(channel, data, past_n_seconds)
+	instance.show_result(res)
+	return res
+
+
+## Schedule a notification loaded from a json.
+static func schedule_json(
+	channel: String,
+	json_name: String,
+	past_n_seconds: int = 0
+) -> String:
+	if !was_setted(): return ""
+	if !validate_channel_id(channel): return ""
+	
+	var res = instance._plugin.scheduleJson(
+		channel,
+		json_name,
+		past_n_seconds,
+	)
+	instance.show_result(res)
+	return res
+
+
+## Cancel a scheduled notificaton o close it if it was already sended.
+static func cancel(
+	notification_id: int
+) -> String:
+	if !was_setted(): return ""
+	
+	var res = instance._plugin.cancel(notification_id)
+	instance.show_result(res)
+	return res
+
+# # Plugin interfaces # #
+
+func _ready() -> void:
+	if instance:
+		return printerr("This class should only be instantiated once!")
+	self.plugin_exists()
+	
+	# Save images.
+	if !on_editor:
+		for image: Imagen in images:
+			var img = image.texture.get_image()
+			var err = img.save_png("user://" + image.save_name)
+			if err != OK:
+				print("error: ", image.save_name)
+	
+	self.call_deferred("setup")
+	instance = self
+
+
+static func validate_channel_id(id: String) -> bool:
+	var res = instance.channels.find_custom(
+		func(ch: Channel): 
+			return ch.channel_id == id
+	)
+	var valid = res < 0
+	if valid:
+		var ids = instance.channels.map(func(ch: Channel): return ch.channel_id)
+		printerr("Invalid id [%d]. Ids: [%s]" % id % ids)
+	return valid
+
+
+static func was_setted() -> bool:
+	if !instance:
+		printerr("Instance must be setted!")
+		return false
+	elif !instance.setup_flag:
+		printerr("Setup must be called first!")
+	return instance.setup_flag
+
+
+func plugin_exists() -> void:
+	var _plugin_name: String = "NotificationHandler"
+	if Engine.has_singleton(_plugin_name):
+		_plugin = Engine.get_singleton(_plugin_name)
+	else:
+		printerr("Couldn't find plugin " + _plugin_name)
+
+
+
+func show_result(res):
+	print("Show: ", res)
--- main.gd
+++ main.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @export_group("Send Notification")
-@export var channel_index: int = 0
+@export var channel: String = ""
 @export var notification_object: Notification = Notification.new()
 @export var delay: int = 0
 @export_group("")
@@ -19,20 +19,61 @@ func _init_sizes():
 
 
 func _on_send_notification_pressed() -> void:
-	$NotificationHandler.trigger_notification(
-		channel_index,
+	NotificationNode.trigger_notification(
+		channel,
 		notification_object,
-		delay,
 	)
 
+
 func _on_image_pressed() -> void:
-	$NotificationHandler.trigger_notification(
-		channel_index,
+	NotificationNode.schedule(
+		channel,
 		NotificationWithImage.new()
 			.set_image("small_icon.png")
 			.set_basic_info("With Image", "Content with image", "small_icon.png", Notification.Priority.HIGH),
 		delay,
 	)
+
+
+func _on_json_pressed() -> void:
+	var init_noti = (NotificationLines.new()
+		.set_lines(['New notification', 'NOT UPDATED'])
+		.set_basic_info(
+			"JSON",
+			"JSON test",
+			"small_icon.png",
+			Notification.Priority.HIGH
+	))
+	var json_name = JSONHandler.get_json_path("on_json_pressed")
+	JSONHandler.save_json(json_name, init_noti.as_dictionary())
+	NotificationNode.schedule_json(channel, json_name, delay)
+	init_noti.set_lines(['New NEW notification', 'JSON UPDATED!!!'] as Array[String])
+	JSONHandler.save_json(json_name, init_noti.as_dictionary()) ## TODO
+
+
+var aux_id: int = 0
+func _on_create_pressed() -> void:
+	var res = NotificationNode.schedule(
+		channel,
+		NotificationBigText.new()
+			.set_big_text("This notification could have been canceled... but it survived.")
+			.set_basic_info(
+				"The survivor",
+				"#1",
+				"small_icon.png",
+				Notification.Priority.HIGH,
+			),
+		delay,
+	)
+	var data = JSON.parse_string(res)
+	var key = NotificationNode.NOTIFICATION_ID
+	if data.has(key):
+		aux_id = data[key]
+
+
+func _on_delete_pressed() -> void:
+	NotificationNode.cancel(aux_id)
+	aux_id = 0
 
 
 func _on_exit_pressed() -> void:
