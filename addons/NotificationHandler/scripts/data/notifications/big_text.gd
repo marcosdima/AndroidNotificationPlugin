bd1f83e94f0b17e5a98f485f8f17c38af93befe5
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-09-19 11:31:35 -0300

    fix: missing setters

--- addons/NotificationHandler/scripts/data/notifications/big_text.gd
+++ addons/NotificationHandler/scripts/data/notifications/big_text.gd
@@ -4,6 +4,10 @@ class_name NotificationBigText
 const BIG_TEXT = "bigText"
 @export var big_text: String = ""
 
+func set_big_text(str: String) -> NotificationBigText:
+	big_text = str
+	return self
+
 func as_dictionary() -> Dictionary:
 	var aux = super()
 	aux[BIG_TEXT] = big_text
--- addons/NotificationHandler/scripts/data/notifications/lines.gd
+++ addons/NotificationHandler/scripts/data/notifications/lines.gd
@@ -4,6 +4,10 @@ class_name NotificationLines
 const LINES = "lines"
 @export var lines: Array[String] = []
 
+func set_lines(new_lines: Array[String]) -> NotificationLines:
+	lines = new_lines
+	return self
+
 func as_dictionary() -> Dictionary:
 	var aux = super()
 	aux[LINES] = lines
